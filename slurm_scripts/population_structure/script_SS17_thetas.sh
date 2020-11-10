39c877cca6174ecccd6ece9282cafb2140a59e10
joemcgirr <46727259+joemcgirr@users.noreply.github.com>
2020-11-10 15:11:41 -0800

    update

--- slurm_scripts/population_structure/script_SS17_thetas.sh
+++ slurm_scripts/population_structure/script_SS17_thetas.sh
@@ -10,14 +10,14 @@
 #SBATCH -p high 
 
 /home/jamcgirr/apps/angsd_sep_20/angsd/angsd -bam /home/jamcgirr/ph/data/angsd/SFS/thetas/SS17_bams_p_1_5_rm.txt -doSaf 1 -doMajorMinor 1 -doMaf 3 -anc /home/jamcgirr/ph/data/c_harengus/c.harengus.fa -minMapQ 30 -minQ 20 -GL 1 -P 8 -out /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30 
-/home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -P 8 -nSites 100000 > /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_unfolded.sfs 
-/home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS saf2theta /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -outname /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30 -sfs /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_unfolded.sfs 
-/home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30.thetas.idx 
-/home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30.thetas.idx -win 50000 -step 10000 -outnames /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_50kb_win_10kb_step 
+#/home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -P 8 -nSites 100000 > /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_unfolded.sfs 
+#/home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS saf2theta /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -outname /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30 -sfs /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_unfolded.sfs 
+#/home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30.thetas.idx 
+#/home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30.thetas.idx -win 50000 -step 10000 -outnames /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30_50kb_win_10kb_step 
 /home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -P 8 -nSites 100000 -fold 1 > /home/jamcgirr/ph/data/angsd/SFS/thetas/folded/population_SS17_minQ20_minMQ30_folded.sfs 
 /home/jamcgirr/apps/angsd_sep_20/angsd/misc/realSFS saf2theta /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.idx -outname /home/jamcgirr/ph/data/angsd/SFS/thetas/unfolded/population_SS17_minQ20_minMQ30 -fold 1 -sfs /home/jamcgirr/ph/data/angsd/SFS/thetas/folded/population_SS17_minQ20_minMQ30_folded.sfs 
 /home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/folded/population_SS17_minQ20_minMQ30.thetas.idx 
 /home/jamcgirr/apps/angsd_sep_20/angsd/misc/thetaStat do_stat /home/jamcgirr/ph/data/angsd/SFS/thetas/folded/population_SS17_minQ20_minMQ30.thetas.idx -win 50000 -step 10000 -outnames /home/jamcgirr/ph/data/angsd/SFS/thetas/folded/population_SS17_minQ20_minMQ30_50kb_win_10kb_step 
-rm /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.gz
+#rm /home/jamcgirr/ph/data/angsd/SFS/thetas/population_SS17_minQ20_minMQ30.saf.gz
 
 #command to run: sbatch script_SS17_thetas.sh
