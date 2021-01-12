c52158d42b09e4c201b5a0c7a9c19024d68a2d30
joemcgirr <46727259+joemcgirr@users.noreply.github.com>
2021-01-11 17:19:56 -0800

    update

--- slurm_scripts/fastq_to_vcf/script_TB06_subset_snps_by_pop.sh
+++ slurm_scripts/fastq_to_vcf/script_TB06_subset_snps_by_pop.sh
@@ -13,7 +13,7 @@ module load samtools
 module load bcftools 
 bcftools view -S /home/jamcgirr/ph/scripts/angsd/SFS/SFS_by_pop/TB06_plates_1_through_5_rm.txt -O z /home/jamcgirr/ph/data/combine_gvcfs/merged_filtered_snps.bcf > /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_snps.vcf.gz 
 module load vcftools 
-vcftools --gzvcf /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_snps.vcf.gz --max-missing 0.25 --recode-INFO-all --recode --stdout | bgzip -c > /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_filtered_snps.vcf.gz 
+vcftools --gzvcf /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_snps.vcf.gz --max-missing 0.5 --recode-INFO-all --recode --stdout | bgzip -c > /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_filtered_snps.vcf.gz 
 bcftools index /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_filtered_snps.vcf.gz 
 
 
--- slurm_scripts/fastq_to_vcf/script_merge_filtered_pops.sh
+++ slurm_scripts/fastq_to_vcf/script_merge_filtered_pops.sh
@@ -11,15 +11,15 @@
 
 module load samtools 
 module load bcftools 
-bcftools merge -O v --threads 16 -o /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.vcf /home/jamcgirr/ph/data/combine_gvcfs/population_BC17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_CA17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS07_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS91_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS06_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB91_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_WA17_filtered_snps.vcf.gz 
+bcftools merge -O v --threads 16 -o /home/jamcgirr/ph/data/vcfs/ph_test_delete.vcf /home/jamcgirr/ph/data/combine_gvcfs/population_BC17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_CA17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS07_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS91_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_PWS96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS06_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_SS96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB06_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB17_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB91_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_TB96_filtered_snps.vcf.gz /home/jamcgirr/ph/data/combine_gvcfs/population_WA17_filtered_snps.vcf.gz 
 
-wc -l /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.vcf > lines_ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.txt 
-module load vcftools 
-vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.vcf --BEAGLE-PL --chr chr1 --out /home/jamcgirr/ph/data/vcfs/chr1_ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25 
-#vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.vcf --BEAGLE-PL --chr chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chr23,chr24,chr25,chr26  --out /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25 
-vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25.vcf --plink --out /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25 
-module load plink 
-plink --file /home/jamcgirr/ph/data/vcfs/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25 --pca --out /home/jamcgirr/ph/data/plink/ph_filtered_snps_minDP600_maxDP2000_minQ20_minMQ30_maxmiss0.25
+wc -l /home/jamcgirr/ph/data/vcfs/ph_test_delete.vcf > lines_ph_test_delete.txt 
+#module load vcftools 
+#vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_test_delete.vcf --BEAGLE-PL --chr chr1 --out /home/jamcgirr/ph/data/vcfs/chr1_ph_test_delete 
+#vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_test_delete.vcf --BEAGLE-PL --chr chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chr23,chr24,chr25,chr26  --out /home/jamcgirr/ph/data/vcfs/ph_test_delete 
+#vcftools --vcf /home/jamcgirr/ph/data/vcfs/ph_test_delete.vcf --plink --out /home/jamcgirr/ph/data/vcfs/ph_test_delete 
+#module load plink 
+#plink --file /home/jamcgirr/ph/data/vcfs/ph_test_delete --pca --out /home/jamcgirr/ph/data/plink/ph_test_delete
 
 
 #run: sbatch script_merge_filtered_pops.sh
